ec7019ba1f406f1ea9b07e3536a029705672bdd5
dbeley <6568955+dbeley@users.noreply.github.com>
2019-05-16 14:19:20 +0200

    update get_discography

--- get_discography.py
+++ get_discography.py
@@ -19,20 +19,38 @@ browser = webdriver.Firefox(options=options)
 
 
 def get_soup(browser, url):
-    logger.info(f"URL : {url}")
-
+    logger.debug('get_soup(browser, %s)', url)
     # if cookie bar found, click on the ok button
     try:
         browser.find_element_by_class_name('as-oil__btn-optin').click()
         logger.debug("Cookie bar found. Clicking on ok.")
         # time.sleep(random.uniform(1, 2))
     except Exception as e:
-        logger.debug(f"Cookie bar not found, {e}")
+        logger.debug("Cookie bar not found, %s", str(e).strip())
 
-    logger.debug(f"browser.get({url})")
+    logger.debug("browser.get(%s)", url)
     browser.get(str(url))
 
     soup = BeautifulSoup(browser.page_source, 'lxml')
+    with open(f"Exports/last_soup.html", 'w') as f:
+        f.write(soup.prettify())
+    try:
+        # rate-limit
+        if soup.find('form', {'id': 'sec_verify'}):
+            logger.error("Rate-limit detected. Restarting browser at %s", url)
+            # with open(f"{export_directory}/ratelimit_detected.html", 'w') as f:
+            #     f.write(soup.prettify())
+            browser.quit()
+            browser = webdriver.Firefox(options=options)
+            soup.decompose()
+            soup = get_soup(browser, url)
+
+    except Exception as e:
+        logger.error("Error scraping page %s : %s", url, e)
+        exit()
+        # with open(f"{export_directory}/soup_error.html", 'w') as f:
+        #     f.write(soup.prettify())
+        # break
     return soup
 
 
@@ -40,16 +58,20 @@ def get_urls_from_artists_name(browser, list_artists):
     list_urls = []
     base_url = "https://rateyourmusic.com"
     for index, artist in enumerate(list_artists, 1):
+        artist = artist.replace(' ', '+')
         url = f"{base_url}/search?searchtype=a&searchterm={artist}"
+        logger.debug("Searching %s in url %s", artist, url)
         soup = get_soup(browser, url)
-        list_urls.append(f"{base_url}{soup.find('a', {'class': 'searchpage'})['href']}")
+        url_artist = f"{base_url}{soup.find('a', {'class': 'searchpage'})['href']}"
+        logger.info("%s found : %s", artist, url_artist)
+        list_urls.append(url_artist)
     return(list_urls)
 
 
 def get_artist_disco(soup):
     artist_disco = []
     artist = soup.find('h1', {'class': 'artist_name_hdr'}).text.strip()
-    logger.debug(artist)
+    logger.debug('Extracting discography for %s', artist)
     disco = soup.find('div', {'id': 'discography'})
     sections = disco.find_all('div', {'class': 'disco_header_top'})
     for section in sections:
@@ -68,11 +90,12 @@ def get_artist_disco(soup):
             dict_disc['Average Rating'] = disc.find('div', {'class': 'disco_avg_rating'}).text
             dict_disc['Ratings'] = disc.find('div', {'class': 'disco_ratings'}).text
             dict_disc['Reviews'] = disc.find('div', {'class': 'disco_reviews'}).text
-            logger.debug(dict_disc)
+            # logger.debug('Final dict for %s : %s', artist, dict_disc)
 
+            logger.debug('Getting information for disc %s - %s - %s', artist, album.text.strip(), date.text.strip())
             try:
                 dict_disc_complementary = get_complementary_disc_infos(disc, dict_disc['URL'], dict_disc['Year'])
-                logger.debug(dict_disc_complementary)
+                # logger.debug('Complementary dict extracted : %s', dict_disc_complementary)
                 dict_disc.update(dict_disc_complementary)
             except Exception as e:
                 logger.error(e)
@@ -113,31 +136,35 @@ def main():
     separate_export = args.separate_export
 
     if not any([url, artist, file_url, file_artist]):
-        logger.error("Not enought arguments")
+        logger.error("Not enought arguments. Use -h to see available arguments.")
         exit()
     if url:
-        list_urls = [x.strip() for x in url.split(',')]
+        list_urls = [x.strip() for x in url.split(',') if x.strip()]
+        logger.debug("Option url found, list_urls : %s", list_urls)
     if file_url:
         try:
             with open(file_url) as f:
-                list_urls = [x.strip() for x in f.readlines()]
+                list_urls = [x.strip() for x in f.readlines() if x.strip()]
         except Exception as e:
             logger.error(e)
             exit()
+        logger.debug("Option file_url found, list_urls : %s", list_urls)
     if artist:
-        list_artists = [x.strip() for x in artist.split(',')]
+        list_artists = [x.strip() for x in artist.split(',') if x.strip()]
+        logger.debug("Option artist found, list_artists : %s", list_artists)
     if file_artist:
         try:
             with open(file_artist) as f:
-                list_artists = [x.strip() for x in f.readlines()]
+                list_artists = [x.strip() for x in f if x.strip()]
         except Exception as e:
             logger.error(e)
             exit()
+        logger.debug("Option file_artist found, list_artists : %s", list_artists)
 
     if list_artists:
         list_urls = get_urls_from_artists_name(browser, list_artists)
 
-    logger.debug(list_urls)
+    logger.debug('final list_urls : %s', list_urls)
     # exit()
     export_directory = "Exports"
     Path(export_directory).mkdir(parents=True, exist_ok=True)
@@ -146,25 +173,9 @@ def main():
 
     list_artists_disco = []
     for url in list_urls:
+        logger.debug('Getting artist discography for url %s', url)
 
         soup = get_soup(browser, url)
-
-        try:
-            # rate-limit
-            if soup.find('form', {'id': 'sec_verify'}):
-                logger.error(f"Rate-limit detected. Restarting browser at {url}")
-                # with open(f"{export_directory}/ratelimit_detected.html", 'w') as f:
-                #     f.write(soup.prettify())
-                browser.quit()
-                browser = webdriver.Firefox(options=options)
-                soup.decompose()
-                soup = get_soup(browser, url)
-
-        except Exception as e:
-            logger.error(f"Error scraping page {url} : {e}")
-            # with open(f"{export_directory}/soup_error.html", 'w') as f:
-            #     f.write(soup.prettify())
-            break
 
         artist_disco = get_artist_disco(soup)
         list_artists_disco.extend(artist_disco)
